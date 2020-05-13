9bfd7ed85f0a4aa1b4b928dcd1ad1b632d02142d
JavierGonzalez <gonzo@virtualpol.com>
2020-05-13 23:09:50 +0200

    +

--- public/info/blocks.php
+++ public/info/blocks.php
@@ -23,7 +23,7 @@ if ($_GET['address'] AND $_GET['address']!='true')
 
 $data = sql("SELECT blockchain, height, hash, pool, pool_link, tx_count, time, hashpower, power_by, 0 AS miners,
     (SELECT COUNT(*) FROM actions WHERE blockchain = blocks.blockchain AND  height = blocks.height) AS actions 
-    ".($_GET['address']?", (SELECT address FROM miners WHERE blockchain = blocks.blockchain AND height = blocks.height ORDER BY quota DESC LIMIT 1) AS address":"")
+    ".($_GET['address']?", (SELECT address FROM miners WHERE blockchain = blocks.blockchain AND height = blocks.height ORDER BY hashpower DESC LIMIT 1) AS address":"")
     .($_GET['coinbase']?", coinbase":"")."
     FROM blocks ".($sql_where?"WHERE ".implode(" AND ", $sql_where):"")."
     ORDER BY time DESC, height DESC");
